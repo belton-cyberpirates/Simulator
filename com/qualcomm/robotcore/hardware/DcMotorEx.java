ebd1941ee39e76a75cb0aa1c9be0c6b4da3d87bd
Oshawhat <alannobledall@gmail.com>
2024-10-08 14:35:09 -0500

    create variables for current position, target position, velocity, and power, and set them in their respective methods

--- com/qualcomm/robotcore/hardware/DcMotorEx.java
+++ com/qualcomm/robotcore/hardware/DcMotorEx.java
@@ -4,36 +4,49 @@ import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.HardwareDevice;
 
 public class DcMotorEx extends HardwareDevice {
+    private final static int TARGET_COMFORT_DIST = 10;
+
     private DcMotor.RunMode runMode = DcMotor.RunMode.DEFAULT;
     private DcMotor.ZeroPowerBehavior zeroPowerBehavior = DcMotor.ZeroPowerBehavior.DEFAULT;
+    
+    private int currentPosition = 0;
+    private int targetPosition;
+    private int velocity;
+    private double power;
 
     public DcMotorEx(String name) {
         super(name);
     }
 
-    public void setTargetPosition(int position) {
-        assert (position != 0 && this.runMode != DcMotor.RunMode.RUN_TO_POSITION) : 
+    public void setTargetPosition(int targetPosition) {
+        assert (targetPosition != 0 && this.runMode != DcMotor.RunMode.RUN_TO_POSITION) : 
             "Trying to set non-zero target position before setting runMode to RUN_TO_POSITION.\nTry calling motor.setMode(DcMotor.RunMode.RUN_TO_POSITION);";
-        System.out.println("moving " + this.name + " to " + position);
+        
+        this.targetPosition = targetPosition;
+        System.out.println("moving " + this.name + " to " + targetPosition);
     }
 
     public void setVelocity(int velocity) {
+        this.velocity = velocity;
         System.out.println("Setting " + this.name + "'s velocity to " + velocity);
     }
 
     public void setPower(double power) {
+        this.power = power;
         System.out.println("Setting " + this.name + "'s power to " + power);
     }
 
     public void setMode(DcMotor.RunMode runMode) {
+        this.runMode = runMode;
         System.out.println("Setting " + this.name + "'s run mode to " + runMode);
     }
 
-    public void setZeroPowerBehavior(DcMotor.ZeroPowerBehavior behavior) {
-        System.out.println("Setting " + this.name + "'s zero power behavior to " + behavior);
+    public void setZeroPowerBehavior(DcMotor.ZeroPowerBehavior zeroPowerBehavior) {
+        this.zeroPowerBehavior = zeroPowerBehavior;
+        System.out.println("Setting " + this.name + "'s zero power behavior to " + zeroPowerBehavior);
     }
 
     public boolean isBusy() {
-        return false;
+        return Math.abs(this.targetPosition - this.currentPosition) <= TARGET_COMFORT_DIST;
     }
 }
