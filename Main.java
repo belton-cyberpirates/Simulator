0bfbdfac9584dcf5ad8877b91d98b3be0cbb1219
Oshawhat <alannobledall@gmail.com>
2024-10-11 17:27:29 -0500

    update Main class

--- Main.java
+++ Main.java
@@ -3,7 +3,7 @@ import simulatorlib.Simulator;
 public class Main {
     public static void main(String[] args) {
         Simulator simulator = new Simulator();
-        YourAuto auto = new YourAuto(); // Replace YourAuto with your auto class
+        AutoBlueLeft auto = new AutoBlueLeft(); // Replace AutoBlueLeft with your auto class
         auto.runOpMode();
     }
 }
