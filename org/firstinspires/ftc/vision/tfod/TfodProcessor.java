f03ac616d1b8bb55c9b150b5bba9148d37ddada1
Oshawhat <alannobledall@gmail.com>
2024-10-22 13:11:59 -0500

    build out tfodprocessor class

--- org/firstinspires/ftc/vision/tfod/TfodProcessor.java
+++ org/firstinspires/ftc/vision/tfod/TfodProcessor.java
@@ -1,5 +1,33 @@
 package org.firstinspires.ftc.vision.tfod;
 
+import org.firstinspires.ftc.robotcore.external.tfod.Recognition;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class TfodProcessor {
-    
+    public static class Builder {
+        String modelFileName;
+        String[] modelLabels;
+
+        public Builder setModelFileName(String modelFileName) {
+            this.modelFileName = modelFileName;
+            return this;
+        }
+        public Builder setModelLabels(String[] modelLabels) {
+            this.modelLabels = modelLabels;
+            return this;
+        }
+        public TfodProcessor build() {
+            return new TfodProcessor();
+        }
+    }
+
+    public List<Recognition> getRecognitions() {
+        ArrayList<Recognition> recognitions = new ArrayList<Recognition>();
+        for (int i = 0; i<5; i++) {
+            recognitions.add(new Recognition());
+        }
+        return recognitions;
+    }
 }
