e22e72fa6e0879c5656314ccaf9c350782a9b31d
Oshawhat <alannobledall@gmail.com>
2024-10-22 13:15:20 -0500

    add hardware configs

--- Main.java
+++ Main.java
@@ -1,9 +1,23 @@
+import org.firstinspires.ftc.teamcode.AutoBlueLeft;
+
 import simulatorlib.Simulator;
+import simulatorlib.hardwareconfigs.*;
 
 public class Main {
     public static void main(String[] args) {
-        Simulator simulator = new Simulator();
-        AutoBlueLeft auto = new AutoBlueLeft(); // Replace AutoBlueLeft with your auto class
-        auto.runOpMode();
+        new Simulator();
+
+        // Hardware Configs
+        // Enter a hardware device like the following:
+        // DcMotorExConfig("motor1", 1/20);
+        // This adds a DcMotorEx with the config name 
+        // "motor1" and a gear ratio of 20:1
+
+        new DcMotorExConfig("m1", 1.0/20.0);
+        new DcMotorExConfig("m2", 1.0/20.0);
+        new DcMotorExConfig("m3", 1.0/20.0);
+        new DcMotorExConfig("m4", 1.0/20.0);
+
+        ( new AutoBlueLeft() ).runOpMode(); // Replace AutoBlueLeft with your auto class
     }
 }
