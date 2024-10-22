816cb8835b21fe59d0b4614bf098574e08eb09b2
Oshawhat <alannobledall@gmail.com>
2024-10-22 13:11:12 -0500

    add group string to auto @interface

--- com/qualcomm/robotcore/eventloop/opmode/Autonomous.java
+++ com/qualcomm/robotcore/eventloop/opmode/Autonomous.java
@@ -2,5 +2,6 @@ package com.qualcomm.robotcore.eventloop.opmode;
 
 public @interface Autonomous {
     public String name() default "";
+    public String group() default "";
     public String preselectTeleOp() default "";
 }
